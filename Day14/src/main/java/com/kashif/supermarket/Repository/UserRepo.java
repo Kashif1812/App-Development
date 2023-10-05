4458f099ed10ac0b0d8d9f31310f3c262b7a9e83
Kashif1812 <110534783+Kashif1812@users.noreply.github.com>
2023-10-05 15:27:35 +0530

    Add files via upload

---
+++ Day14/src/main/java/com/kashif/supermarket/Repository/UserRepo.java
@@ -0,0 +1,12 @@
+package com.kashif.supermarket.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kashif.supermarket.Model.User;
+@Repository
+public interface UserRepo extends JpaRepository<User,Integer> {
+    Optional<User> findByUsername(String username);
+}
